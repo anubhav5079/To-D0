633cdce82bc4154f3323967f51427e01b325d3f6
Anubhav Kumar <anubhav.k@outlook.com>
2023-09-17 17:52:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const listContainer=document.getElementById("list-container")
 
 function addTask(){
   if(inputBox.value===''){
-    alert("likh na bahinchod")
+    alert("Write something ")
   }
   else{
     let li=document.createElement("li");
